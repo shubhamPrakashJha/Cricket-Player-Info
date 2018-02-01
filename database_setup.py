3878e2daf84bf5c1010be2c4d061efac43c71812
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-02-01 11:14:20 +0530

    import configuration file

--- database_setup.py
+++ database_setup.py
@@ -1,5 +1,11 @@
+import os
+import sys
 
-
+# Configuration file
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 
 
 
