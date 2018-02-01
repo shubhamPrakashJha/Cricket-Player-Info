0b6aa9467ffd43c8ed8794f9d7ff00f55ec09687
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-02-01 11:54:34 +0530

    ctrate class(table) for players

--- database_setup.py
+++ database_setup.py
@@ -18,3 +18,21 @@ class Team(Base):
     # Mapper
     name = Column(String(250), nullable=False)
     id = Column(Integer, primary_key=True)
+
+
+class Player(Base):
+    # Table info
+    __tablename__ = 'player'
+
+    # mapper
+    name = Column(String(120), nullable=False)
+    id = Column(Integer, primary_key=True)
+    match = Column(Integer)
+    runs = Column(Integer)
+    high_score = Column(Integer)
+    avg = Column(Integer)
+    wickets = Column(Integer)
+    century = Column(Integer)
+    fifty = Column(Integer)
+    team_id = Column(Integer, ForeignKey('team.id'))
+    team = relationship(Team)
