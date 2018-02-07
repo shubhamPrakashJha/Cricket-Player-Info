44c99692ed4ebb0371b1d092a3d169f7c616d00f
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-02-07 13:39:18 +0530

    added routing for all pages into project.py

--- project.py
+++ project.py
@@ -1,9 +1,47 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "hello world"
+@app.route("/teams/")
+def show_players():
+    return "This page will show all Teams"
+
+
+@app.route("/team/new/")
+def new_team():
+    return "This page will create new Team"
+
+
+@app.route("/team/<int:team_id>/edit/")
+def edit_team(team_id):
+    return "This page will edit Team %s" % team_id
+
+
+@app.route("/team/<int:team_id>/delete/")
+def delete_team(team_id):
+    return "This page will delete Team %s" % team_id
+
+
+@app.route("/team/<int:team_id>/")
+@app.route("/team/<int:team_id>/players/")
+def show_players(team_id):
+    return "This page will show all players of Team %s" % team_id
+
+
+@app.route("/team/<int:team_id>/player/new/")
+def new_player(team_id):
+    return "This page add new player in Team %s" % team_id
+
+
+@app.route("/team/<int:team_id>/player/<int:player_id>/edit/")
+def edit_player(team_id, player_id):
+    return "This page will edit player %s of Team %s" % (player_id, team_id)
+
+
+@app.route("/team/<int:team_id>/player/<int:player_id>/delete/")
+def delete_player(team_id, player_id):
+    return "This page will delete player %s of Team %s" % (player_id, team_id)
 
 
 if __name__ == '__main__':
