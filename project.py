696a057fce2b6026512dacdbd6f1a5d371884350
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-03-20 01:57:25 +0530

    Execute HTTP GET request to REVOKE(officially cancel) current token

--- project.py
+++ project.py
@@ -166,6 +166,29 @@ def gdisconnect():
     print 'User name is: '
     print login_session['username']
 
+    # Execute HTTP GET request to REVOKE(officially cancel) current token
+    url = 'http://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+
+    print 'result is'
+    print result
+
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.', 200))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
 
 @app.route('/teams/JSON')
 def teams_json():
